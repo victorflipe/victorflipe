3a8805f0861e480320770d3433e0184a85477b8f
victorroli <victorfelipe1608@gmail.com>
2024-09-15 00:48:42 -0300

    Centralizing the box main

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,8 +11,10 @@ const Layout = ({PageTitle, children, breadcrumbs, marginFooter, noTitle}) => {
 
             <main className="flex-1 xl:px-[20%] lg:px-[10%] md:px-[5%] pt-[5%] z-10">
                 <img className="hidden md:flex absolute -rotate-90 xl:right-[20%] sm:right-0 xl:w-[10rem] md:w-[8rem] sm:w-[6rem] sm:mt-20 " src={iconSquare}/>
-                <h1 className={`xl:text-5xl md:text-4xl text-3xl sm:pb-14 text-[#F47500] mb-10 font-semibold md:text-left text-center mt-10 px-10 sm:px-0 ${noTitle ? "hidden" : ""}`}>{PageTitle}</h1>
-                {children}
+                <h1 className={`xl:text-5xl md:text-4xl text-3xl sm:pb-14 text-[#F47500] mb-10 font-semibold md:text-left text-center mt-10 px-10 sm:px-0 max-w-[50rem] mx-auto ${noTitle ? "hidden" : ""}`}>{PageTitle}</h1>
+                <div className='max-w-[50rem] mx-auto'>
+                    {children}
+                </div>
             </main>
             
             <img className='absolute xl:pl-[10%] md:w-[8rem] xl:w-[25rem] bottom-[10rem]' src={iconSquare}/>
